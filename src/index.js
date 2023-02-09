5f84c5bd18442930094f8c73d1c42263536593b9
deltall1 <andrey.alekseev.1910@gmail.com>
2023-02-09 10:16:16 +0200

    Fixes

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:3000/api',
+  uri: 'api:/api',
   cache: new InMemoryCache(),
 });
 
